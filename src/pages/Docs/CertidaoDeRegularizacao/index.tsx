86289489d7a01c3ca6e30538116c9718d03688de
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-08-17 14:53:30 -0300

    fix: :rotating_light: Remove React Import

--- src/pages/Docs/CertidaoDeRegularizacao/index.tsx
+++ src/pages/Docs/CertidaoDeRegularizacao/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from "react";
+import { useState, FormEvent } from "react";
 import Docxtemplater from "docxtemplater";
 import PizZip from "pizzip";
 import { saveAs } from "file-saver";
